9d9c06aeffc219394a2283a0330204eff33f17bf
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-23 10:15:59 +0200

    x

--- context/index.tsx
+++ context/index.tsx
@@ -3,20 +3,19 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({
   children,
-  defaultTheme,
-  forcedTheme,
+
+
 }: {
   children: React.ReactNode;
-  defaultTheme: Settings["defaultTheme"];
-  forcedTheme: Settings["forcedTheme"];
+
+
 }) {
   return (
     <ThemeProvider
       enableSystem
       disableTransitionOnChange
       attribute="class"
-      defaultTheme={defaultTheme || "system"}
-      forcedTheme={forcedTheme || undefined}
+
     >
       {children}
     </ThemeProvider>
